f8312ee2d11ae40260956b773396861b09a1ae6a
IronManYG <hussain.yg2010@gmail.com>
2021-01-28 21:47:00 +0300

    Display SleepQuality Data

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -97,11 +97,20 @@ class SleepTrackerFragment : Fragment() {
             }
         })
 
-        // TODO (04) Create a new SleepNightAdapter variable,
+        // (04) Create a new SleepNightAdapter variable,
         // and bind it to the RecyclerView’s Adapter.
+        val adapter = SleepNightAdapter()
 
-        // TODO (05) Create an observer on sleepTrackerViewModel.nights that tells
+        binding.sleepList.adapter = adapter
+
+        //  (05) Create an observer on sleepTrackerViewModel.nights that tells
         // the Adapter when there is new data.
+        sleepTrackerViewModel.nights.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                adapter.data = it
+            }
+
+        })
 
         return binding.root
     }
